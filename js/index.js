40b43a9f7abaddd99b3b70751e66730329de19c6
Musa Maphutha <mumaphujh024@student.wethinkcode.co.za>
2025-11-21 16:01:13 +0200

    Added all functionality on the study tracking application

---
+++ js/index.js
@@ -0,0 +1,31 @@
+let count = 0
+let countEl = document.getElementById("count-session")
+let session = document.getElementById("answer")
+
+function increment(){
+
+    count += 1
+    countEl.textContent = count
+}
+
+function decrement(){
+
+    count -= 1
+    countEl.textContent = count
+}
+
+function save(){
+
+    let results = count + ' - '
+    session.textContent += results
+    count = 0
+    countEl.textContent = count
+}
+
+function clear(){
+
+    let info = ""
+    session.textContent += info
+     
+
+}
